f21d1099dae64023b709c15735788a1c3d02239b
kevincentius <kevincentius@yahoo.com>
2022-05-23 22:10:08 +0200

    Lobby minimum client implementation

--- c3-client/src/app/view/main/main.component.ts
+++ c3-client/src/app/view/main/main.component.ts
@@ -1,9 +1,23 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
+import { MainService } from "app/view/main/main.service";
+import { LobbyComponent } from "app/view/menu/lobby/lobby.component";
+import { RoomComponent } from "app/view/menu/room/room.component";
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
+  providers: [MainService],
 })
 export class MainComponent {
+
+  @ViewChild('lobby', { static: true })
+  private lobby!: LobbyComponent;
+  
+  @ViewChild('room', { static: true })
+  private room!: RoomComponent;
+
+  onEnterRoom(roomId: number) {
+    this.room.show(roomId);
+  }
 }
