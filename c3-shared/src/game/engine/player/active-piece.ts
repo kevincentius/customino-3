d6a9c571b12becee00e575b7567228a0e3ed1057
kevincentius <kevincentius@yahoo.com>
2022-06-17 22:21:39 +0200

    #4 - active piece spawn position auto center

--- c3-shared/src/game/engine/player/active-piece.ts
+++ c3-shared/src/game/engine/player/active-piece.ts
@@ -67,8 +67,8 @@ export class ActivePiece {
 
   spawn(piece: Piece) {
     this.piece = piece;
-    this.x = 3;
-    this.y = this.board.invisibleHeight;
+    this.x = Math.floor((this.board.tiles[0].length - piece.tiles[0].length) / 2);
+    this.y = this.board.invisibleHeight - piece.tiles.length + MatUtil.countEmptyRowsBottom(piece.tiles);
     this.gravityFrameCount = 0;
     this.lockDelayFrameCount = 0;
 
