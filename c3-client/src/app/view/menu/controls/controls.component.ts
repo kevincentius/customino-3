0711d2d3400c58278ced2847a6abc7dd913457ce
kevincentius <kevincentius@yahoo.com>
2022-08-02 21:08:44 +0200

    set controls one key at a time instead of back to back

--- c3-client/src/app/view/menu/controls/controls.component.ts
+++ c3-client/src/app/view/menu/controls/controls.component.ts
@@ -65,10 +65,11 @@ export class ControlsComponent {
       this.rows[this.editIndex].mappings = this.localSettings.control.keyMap.get(inputKey)!;
       this.userSettingsService.save();
       this.editIndex++;
+      this.editIndex = null;
 
-      if (this.editIndex >= this.rows.length) {
-        this.editIndex = null;
-      }
+      // if (this.editIndex >= this.rows.length) {
+      //   this.editIndex = null;
+      // }
     }
   }
 
